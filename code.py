64df4cbf4041835c4867bcfdf751bc80d2036ad9
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-10-12 16:17:02 +0300

    Update code.py

--- code.py
+++ code.py
@@ -283,6 +283,7 @@ async def guild(ctx, guild: discord.Guild = None):
     emb = discord.Embed(title = f'Информация о {guild}', colour = discord.Color.green(), timestamp = ctx.message.created_at)
     emb.add_field(name = 'ID сервера', value = guild.id)
     emb.add_field(name = 'Уровень сервера', value = guild.premium_tier)
+    emb.add_field(name = 'Люди, бустящие сервер', value = guild.premium_subscribers)
     emb.set_footer(text = 'Обратите внимание, что это Бета версия основного бота.')
     emb.set_thumbnail(url = guild.icon_url)
     await ctx.send(embed = emb)
