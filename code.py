4d308f7eee26d4cf0321ed6832d340ede81def2b
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-10-14 21:53:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -32,9 +32,9 @@ class TimeConverter(commands.Converter):
 #test space
 @client.event
 async def on_member_update(before, after):
-    if before.member.roles != after.member.roles:
+    if before.roles != after.roles:
         channel = client.get_channel(714175791033876490)
-        if after.member.top_role > before.member.top_role:
+        if after.top_role > before.top_role:
             emb = discord.Embed(title = 'ВНИМАНИЕ', description = 'БЫЛА ОБНАРУЖЕНА ПОПЫТКА ВЫДАЧИ РОЛИ ВЫШЕ ТОЙ, ЧТО БЫЛА У ЧЕЛОВЕКА.', colour = discord.Color.red())
             emb.add_field(name = 'Выдал роль', value = 'а я ебу? в журнале аудита посмотри, оттуда ничего нельзя удалить')
             await channel.send(f'<@!338714886001524737>', embed = emb)
