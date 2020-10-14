1de009816f5309525d12ade1cd40be9ef937afa7
D-9341 <58139230+D-9341@users.noreply.github.com>
2020-10-14 16:01:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -293,7 +293,7 @@ async def guild(ctx):
     b = guild.created_at
     c = a - b
     d = guild.created_at.strftime('%d/%m/%Y %H:%M:%S UTC')
-    emb.add_field(name = 'Дата создания сервера', value = f'{c.years} лет, {c.months} месяцев, {c.weeks} недель, {c.days} дней ({d})', inline = False)
+    emb.add_field(name = 'Дата создания сервера', value = f'{c.weeks} недель, {c.days} дней ({d})', inline = False)
     emb.set_footer(text = 'Обратите внимание, что это Бета версия основного бота.')
     emb.set_thumbnail(url = guild.icon_url)
     await ctx.send(embed = emb)
